6ec80403c2f6b4d649d414d8b62060ea57322d3a
Piet <pj.mens121@gmail.com>
2021-04-08 16:14:20 +0200

    Yeet

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, g
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms import BooleanField
+import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ditisgeheim'
@@ -11,17 +12,28 @@ class InfoForm(FlaskForm):
     newsletter = BooleanField('Wilt u de nieuwsbrief?')
     submit = SubmitField('Verzenden')
 
+
+def get_db():
+    DATABASE = '../filmfan.db'
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
+    db = get_db().cursor()
+    movies = db.execute('select * from movies').fetchall()
+    print(movies)
     print(request.args)
     print(request.form)
     form = InfoForm()
     if form.validate_on_submit():
         email = form.email.data
-        return render_template('index.html' , email=email)
+        return render_template('index.html', email=email)
 
+    return render_template('index.html', form=form, movies=movies)
 
-    return render_template('index.html', form=form)
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
