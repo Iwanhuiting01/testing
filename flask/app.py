e9502fcbcfb213b567b9205953a4e4f79be29778
Iwan <Iwan.l.huiting@gmail.com>
2021-04-09 10:59:07 +0200

    ghfkhgfjfgc

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, g
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms import BooleanField
+import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ditisgeheim'
@@ -11,51 +12,42 @@ class InfoForm(FlaskForm):
     newsletter = BooleanField('Wilt u de nieuwsbrief?')
     submit = SubmitField('Verzenden')
 
+def get_db():
+    DATABASE = '../filmfan.db'
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    print(request.args)
-    print(request.form)
-    form = InfoForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        return render_template('index.html' , email=email)
+    db = get_db().cursor()
+    movies = db.execute('select * from movies').fetchall()
 
+    return render_template('index.html', movies=movies)
 
-    return render_template('index.html', form=form)
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
-    form = InfoForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        return render_template('login.html')
 
     return render_template('login.html')
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
-    form = InfoForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        return render_template('register.html')
 
     return render_template('register.html')
 
-@app.route('/movie', methods=('GET', 'POST'))
-def movie():
-    form = InfoForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        return render_template('movie.html')
+@app.route('/movie/<id>', methods=('GET', 'POST'))
+def movie(id):
+    db = get_db().cursor()
+    movie = db.execute('select * from movies WHERE id = ?;', (id)).fetchone()
 
-    return render_template('movie.html')
+    director = db.execute('select * from directors WHERE id = ?;', (movie[2])).fetchone()
+
+    return render_template('movie.html', movie=movie, director=director)
 
 @app.route('/movie/create', methods=('GET', 'POST'))
 def create_movie():
-    form = InfoForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        return render_template('create_movie.html')
 
     return render_template('create_movie.html')
 
