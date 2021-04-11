8076eb4b0ac018bba33f82f1573a75642004e0ff
Piet <pj.mens121@gmail.com>
2021-04-11 10:58:06 +0200

    Register

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template, request, url_for, g
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms import BooleanField
+import os
 import sqlite3
+
+from flask import Flask, render_template, g, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import BooleanField
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.join(__file__, "../.."))
+print(basedir)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ditisgeheim'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'filmfan.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Forms
 
 class InfoForm(FlaskForm):
     email = StringField('Wat is uw email?')
@@ -13,20 +26,41 @@ class InfoForm(FlaskForm):
     submit = SubmitField('Verzenden')
 
 
+class RegisterForm(FlaskForm):
+    email = StringField('E-mail:', validators=[DataRequired()])
+    user_name = StringField('Gebruikers naam:', validators=[DataRequired()])
+    password = PasswordField('Wachtwoord:', validators=[DataRequired()])
+    submit = SubmitField('Verzenden')
+
+# Models
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.Text, unique=True)
+    user_name = db.Column(db.Text)
+    password = db.Column(db.Text)
+
+    def __init__(self, email, user_name, password):
+        self.email = email
+        self.user_name = user_name
+        self.password = password
+
+    # def __repr__(self):
+    #     return f"Cursist {self.naam} is {self.leeftijd} jaar oud"
+
+
 def get_db():
     DATABASE = '../filmfan.db'
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
+    db2 = getattr(g, '_database', None)
+    if db2 is None:
+        db2 = g._database = sqlite3.connect(DATABASE)
+    return db2
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    db = get_db().cursor()
-    movies = db.execute('select * from movies').fetchall()
-    print(movies)
-    print(request.args)
-    print(request.form)
+    db2 = get_db().cursor()
+    movies = db2.execute('select * from movies').fetchall()
     form = InfoForm()
     if form.validate_on_submit():
         email = form.email.data
@@ -42,15 +76,25 @@ def login():
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        username = form.user_name.data
+        password = form.password.data
 
-    return render_template('register.html')
+        user = User(email, username, password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
 
 @app.route('/movie/<id>', methods=('GET', 'POST'))
 def movie(id):
-    db = get_db().cursor()
-    movie = db.execute('select * from movies WHERE id = ?;', (id)).fetchone()
+    db2 = get_db().cursor()
+    movie = db2.execute('select * from movies WHERE id = ?;', (id)).fetchone()
 
-    director = db.execute('select * from directors WHERE id = ?;', (movie[2])).fetchone()
+    director = db2.execute('select * from directors WHERE id = ?;', (movie[2])).fetchone()
 
     return render_template('movie.html', movie=movie, director=director)
 
